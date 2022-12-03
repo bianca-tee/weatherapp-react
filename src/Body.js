c4dad76601b5ea3e32b3a1016b3a5673522049a6
bianca-tee <112151662+bianca-tee@users.noreply.github.com>
2022-12-03 16:59:12 +1100

    Moved contents of Search component into body
    Will need to access form data to send to API to get weather data

--- src/Body.js
+++ src/Body.js
@@ -2,14 +2,28 @@ import React from "react";
 import Search from "./Search";
 import City from "./City";
 import Weather from "./Weather";
+import "./Search.css";
 
 export default function Body() {
   return (
     <div className="row Body">
       <div className="col-12 Search">
-        <Search />
-        <Weather defaultCity="Sydney" />
+        <form action="" className="search-form">
+          <input
+            type="text"
+            placeholder="Enter your city"
+            className="city-box"
+            autoFocus="on"
+          />
+          <button type="submit" className="search-button">
+            Search
+          </button>
+        </form>
+        <button className="location-pin">
+          <i className="fa-solid fa-map-pin pin"></i>
+        </button>
       </div>
+      <Weather defaultCity="Sydney" />
     </div>
   );
 }
